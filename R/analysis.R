c7960fe2b8a86e16f76b6b730038d61387d56a4b
donaldcummins <dc533@exeter.ac.uk>
2020-10-22 18:56:50 +0100

    Updated references to directories

--- R/analysis.R
+++ R/analysis.R
@@ -18,7 +18,7 @@ source("definitions.R")
 
 ## get list of processed .nc files
 processed.files <- list.files(
-  path = "~/CMIP6/historical/processed/tas",
+  path = "../CMIP6/historical/processed/tas",
   full.names = TRUE
 )
 
@@ -54,29 +54,29 @@ multimodel.mean <- rowMeans(ensemble.means)[31:165]
 observations <- data.frame(
   ## Berkeley Earth land and ocean
   berkeley.earth = read.table(
-    file = "observations/Land_and_Ocean_summary.txt",
+    file = "../observations/Land_and_Ocean_summary.txt",
     skip = 48
   )[31:165, 2],
   
   ## Cowtan and Way 2.0
   cowtan.and.way = read.table(
-    file = "observations/had4_krig_annual_v2_0_0.txt"
+    file = "../observations/had4_krig_annual_v2_0_0.txt"
   )[31:165, 2],
   
   ## GISTEMP v4
   gistemp  = as.numeric(read.csv(
-    file = "observations/GLB.Ts+dSST.csv",
+    file = "../observations/GLB.Ts+dSST.csv",
     skip = 1
   )$J.D[1:135]),
   
   ## HadCRUT4
   hadcrut = read.table(
-    file = "observations/HadCRUT.4.6.0.0.annual_ns_avg.txt"
+    file = "../observations/HadCRUT.4.6.0.0.annual_ns_avg.txt"
   )[31:165, 2],
   
   ## NOAAGlobalTemp v5
   noaaglobaltemp = read.table(
-    file = "observations/aravg.ann.land_ocean.90S.90N.v5.0.0.202008.asc"
+    file = "../observations/aravg.ann.land_ocean.90S.90N.v5.0.0.202008.asc"
   )[1:135, 2]
 )
 
@@ -96,10 +96,10 @@ ecms <- lapply(level.regressions, fit.ecm)
 null.models <- lapply(ecms, fit.null)
 
 ## bootstrap null model sampling distributions for each dataset
-# set.seed(44)
-# bootstrap.distributions <- lapply(datasets, bootstrap.null)
-# names(bootstrap.distributions) <- datasets
-# save(bootstrap.distributions, file = "boot.rda")
+set.seed(44)
+bootstrap.distributions <- lapply(datasets, bootstrap.null)
+names(bootstrap.distributions) <- datasets
+save(bootstrap.distributions, file = "boot.rda")
 
 ## assemble meta-bootstrap distribution
 load("boot.rda")
